b91a8c8be49b6db4b48c73aa0747440619e4a216
paul_ <pavel.mordukhai@gmail.com>
2024-07-27 11:30:28 +0700

    HFK-02-02

---
+++ src/main/kotlin/chapter_02/ch_02_app_02_array.kt
@@ -0,0 +1,12 @@
+package org.example.chapter_02
+
+fun main() {
+
+    var myArray = arrayOf(1, 2, 3)
+
+    // получение размера массива
+    println("Размер массива - ${myArray.size}")
+
+    // получение значения элемента массива по индексу
+    println("Элемент под индексом 1: ${myArray[1]}")
+}
