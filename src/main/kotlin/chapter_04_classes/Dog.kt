460714eb1f20b784f9698053d8770907843b56f8
paul_ <pavel.mordukhai@gmail.com>
2024-08-01 16:09:11 +0700

    HFK-04-Dog

---
+++ src/main/kotlin/chapter_04_classes/Dog.kt
@@ -0,0 +1,13 @@
+package org.example.chapter_04_classes
+
+class Dog (val name: String, var weight: Int, val breed: String) {
+
+    fun printBark() {
+        println(if (weight < 20) "Yip!" else "Woof!")
+    }
+
+    fun bark(): String {
+        val song = if (weight < 20) "Yip!" else "Woof!"
+        return song
+    }
+}
