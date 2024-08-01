02147350ae0902680014234a4ca111da692ce147
paul_ <pavel.mordukhai@gmail.com>
2024-08-01 17:50:41 +0700

    HFK-04-Dog

---
+++ src/main/kotlin/chapter_04_classes/duck/Duck.kt
@@ -0,0 +1,14 @@
+package org.example.chapter_04_classes.duck
+
+class Duck {
+
+    fun quack() {
+        println("Quack! Quack! Quack!")
+    }
+}
+
+
+fun main() {
+    val myDuck = Duck()
+    myDuck.quack()
+}
