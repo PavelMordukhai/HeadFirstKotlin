bab140b291c6ddb92763931723209804d4220c34
paul_ <pavel.mordukhai@gmail.com>
2024-08-05 18:26:47 +0700

    HFK-06-polymorphism

---
+++ src/main/kotlin/chapter_07_gata_classes/tests/test_01.kt
@@ -0,0 +1,51 @@
+package org.example.chapter_07_gata_classes.tests
+
+class Recipe1(
+    val title: String,
+    val isVegetarian: Boolean,
+) {
+}
+
+// переопределяет функции hashCode(), toString(), equals() ( == )
+data class RecipeData(
+    val title: String,
+    val isVegetarian: Boolean,
+) {
+}
+
+
+fun main() {
+
+    val r1 = Recipe1("Chicken Bhuna", false)
+    var r2 = Recipe1("Chicken Bhuna", false)
+
+    println(r1 == r2) // false
+    println(r1.hashCode()) // 885284298
+    println(r2.hashCode()) // 1389133897 разное значение
+
+    println(r1.toString()) // Recipe1@34c45dca
+    println(r2.toString()) // Recipe1@52cc8049 разное значение
+
+    r2 = r1
+
+    println(r1 == r2) // true
+    println(r1.hashCode()) // 885284298
+    println(r2.hashCode()) // одинаковое значение
+
+    println(r1.toString()) // Recipe1@34c45dca
+    println(r2.toString()) // одинаковое значение
+    println()
+
+    //
+
+    val r3 = RecipeData("Chicken Bhuna", false)
+    val r4 = RecipeData("Chicken Bhuna", false)
+
+    println(r3 == r4) // true
+    println(r3.hashCode()) // 241132350
+    println(r4.hashCode()) // одинаковое значение
+
+    println(r3.toString()) // Recipe2(title=Chicken Bhuna, isVegetarian=false)
+    println(r4.toString()) // одинаковое значение
+    println()
+}
---
+++ src/main/kotlin/chapter_07_gata_classes/tests/test_02.kt
@@ -0,0 +1,31 @@
+package org.example.chapter_07_gata_classes.tests
+
+fun main() {
+    // Создание копии объекта данных, изменяя некоторые из его свойств,
+    // но оставить другие свойства в исходном состоянии - copy()
+
+    val r1 = RecipeData("Thai Curry", false,)
+    // Создание копии объекта Recipe, заменяя значение свойства isVegetarian
+    val r2 = r1.copy(isVegetarian = true)
+
+    //
+    // Деструктуризация. Функция componentN()
+
+    val r = RecipeData("Chicken Bhuna", false)
+//    val title = r.title
+    // component1() возвращает ссылку, которая содержится в первом свойстве,
+    // определенном в конструкторе класса данных
+    val title1 = r.component1()
+    println(title1)
+    println()
+
+//    val title = r.title
+//    val vegetarian = r.isVegetarian
+
+//    val title = r.component1()
+//    val vegetarian = r.component2()
+
+    val (title, vegetarian) = r
+    println(title)
+    println(vegetarian)
+}
